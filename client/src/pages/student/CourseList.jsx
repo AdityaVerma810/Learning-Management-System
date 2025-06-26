60531cc3edfde619ad37ba04571734506686d85d
AdityaVerma810 <adicode810@gmail.com>
2025-06-26 23:36:18 +0530

    completed course detail page

--- client/src/pages/student/CourseList.jsx
+++ client/src/pages/student/CourseList.jsx
@@ -1,41 +1,62 @@
-import React, { useContext } from 'react'
-import {AppContext} from '../../context/AppContext'
+import React, { useContext, useState, useEffect } from 'react'
+import { AppContext } from '../../context/AppContext'
 import SearchBar from '../../components/students/SearchBar'
 import { useParams } from 'react-router-dom'
 import CourseCard from '../../components/students/CourseCard'
+import { assets } from '../../assets/assets'
+import Footer from '../../components/students/Footer'
+
 
 const CourseList = () => {
+	const { navigate, allCourses } = useContext(AppContext)
+	const { input } = useParams()
+	const [filteredCourse, setFilteredCourse] = useState([])
 
-	const {navigate,allCourses}=useContext(AppContext)
-	const {input}=useParams()
-	const [filteredCourse,setFilteredCourse]=useState([])
+	useEffect(() => {
+		if (allCourses && allCourses.length > 0) {
+			const tempCourses = allCourses.slice()
 
-	useEffect(()=>{
-		if(allCourses && allCourses.length >0){
-			const tempCourses=allCourses.slice()
-
-			
+			if (input) {
+				setFilteredCourse(
+					tempCourses.filter(item =>
+						item.courseTitle.toLowerCase().includes(input.toLowerCase())
+					)
+				)
+			} else {
+				setFilteredCourse(tempCourses)
+			}
 		}
-	},[allCourses,input])
+	}, [allCourses, input])
 
-
-  return (
-	<>
-	<div className='relative md:px-36 px-8 pt-20 text-left'>
-		<div className='flex md:flex-row flex-col gap-6 items-start justify-between w-full'>
-			<div>
-			<h1 className='text-4xl font-semibold text-gray-800'>Course List</h1>
-			<p className='text-gray-500'>
-				<span className='text-blue-600 cursor-pointer' onClick={()=> navigate('/')}>Home</span> /<span> course List </span></p>
+	return (
+		<>
+		<div className='relative md:px-36 px-8 pt-20 text-left'>
+			<div className='flex md:flex-row flex-col gap-6 items-start justify-between w-full'>
+				<div>
+					<h1 className='text-4xl font-semibold text-gray-800'>Course List</h1>
+					<p className='text-gray-500'>
+						<span className='text-blue-600 cursor-pointer' onClick={() => navigate('/')}>Home</span> /<span> course List </span>
+					</p>
+				</div>
+				<SearchBar data={input} />
+			</div>
+			{
+				input && <div className='inline-flex items-center gap-4 px-4 py-2 border mt-8 -mb-8 text-gray-600'>
+					<p>{input}</p>
+					<img src={assets.cross_icon } alt="" className='cursor-pointer' onClick={()=>
+						navigate('/course-list')
+					} />
+				</div>
+			}
+			<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-16 gap-3 px-2 md:p-0'>
+				{filteredCourse.map((course, index) => (
+					<CourseCard key={index} course={course} />
+				))}
+			</div>
 		</div>
-		<SearchBar data={input}/>
-		</div>
-		<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg-grid-cols-4 my-16 gap-3 px-2 md:p-0'>
-			{allCourses.map((course,index)=> <CourseCard key={index} course={course}/>)}
-		</div>
-	</div>
-	</>
-  )
+		<Footer/>
+		</>
+	)
 }
 
 export default CourseList
