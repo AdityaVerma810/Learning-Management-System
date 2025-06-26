58780923479d4ea37fb1f7ce61ece3204699c748
AdityaVerma810 <adicode810@gmail.com>
2025-06-26 23:54:21 +0530

    commited few changes in course detail page

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../../context/AppContext'
 
 const CourseDetails = () => {
+  const { id } = useParams()
+  const [courseData, setCourseData] = useState(null)
+  const { allCourses } = useContext(AppContext)
+
+  const fetchCourseData = async () => {
+    const findCourse = allCourses.find(course => course._id === id)
+    setCourseData(findCourse)
+  }
+
+  useEffect(() => {
+    fetchCourseData()
+  }, [id, allCourses])
+
   return (
-	<div>
-	  <h1>Course Details PAges</h1>
-	</div>
+    <div>
+      <h1>Course Details Page</h1>
+      {courseData ? (
+        <div>
+          <h2>{courseData.title}</h2>
+          <p>{courseData.description}</p>
+          
+        </div>
+      ) : (
+        <p>Loading course details...</p>
+      )}
+    </div>
   )
 }
 
