eeeb792b2eed64c77c631d5b6fe332178cc22479
AdityaVerma810 <adicode810@gmail.com>
2025-06-27 16:38:56 +0530

    adding rating to course detail page

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -7,7 +7,7 @@ import Loading from '../../components/students/Loading';
 const CourseDetails = () => {
   const { id } = useParams();
   const [courseData, setCourseData] = useState(null);
-  const { allCourses } = useContext(AppContext);
+  const { allCourses, calculateRating } = useContext(AppContext);
 
   const fetchCourseData = async () => {
     const findCourse = allCourses.find(course => course._id === id);
@@ -18,7 +18,12 @@ const CourseDetails = () => {
     fetchCourseData();
   }, [id, allCourses]);
 
-  return courseData ? (
+  if (!courseData) return <Loading />;
+
+  const rating = calculateRating(courseData);
+  const ratingCount = courseData.courseRatings?.length || 0;
+
+  return (
     <>
       <div className='flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left'>
         <div className='absolute top-0 left-0 w-full h-section-height -z-1 bg-gradient-to-b from-cyan-100/70'></div>
@@ -26,16 +31,32 @@ const CourseDetails = () => {
           <h1 className='md:text-course-deatails-heading-large text-course-deatails-heading-small font-semibold text-gray-800'>
             {courseData.courseTitle}
           </h1>
+
           <p
+            className='pt-4 text-base text-sm'
             dangerouslySetInnerHTML={{
               __html: (courseData.courseDescription || '').slice(0, 200),
             }}
           ></p>
+
+          {/* Review and Rating */}
+          <div className='flex items-center space-x-2 mt-4'>
+            <p className='font-semibold text-gray-700'>{rating.toFixed(1)}</p>
+            <div className='flex'>
+              {[...Array(5)].map((_, i) => (
+                <img
+                  key={i}
+                  src={i < Math.floor(rating) ? assets.star : assets.star_blank}
+                  alt=''
+                  className='w-3.5 h-3.5'
+                />
+              ))}
+            </div>
+            <p className='text-gray-500 '>{courseData.courseRatings.length} {courseData.courseRatings.length >1 ? 'ratings ': 'rating'}</p>
+          </div>
         </div>
       </div>
     </>
-  ) : (
-    <Loading />
   );
 };
 
