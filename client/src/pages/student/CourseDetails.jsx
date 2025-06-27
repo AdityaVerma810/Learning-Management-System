5b367abe633b2bfb091d52bb40b46f9e8455b592
AdityaVerma810 <adicode810@gmail.com>
2025-06-27 15:45:05 +0530

    doing changes in course fetail page

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -1,35 +1,42 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { AppContext } from '../../context/AppContext'
+import React, { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { AppContext } from '../../context/AppContext';
+import { assets } from '../../assets/assets';
+import Loading from '../../components/students/Loading';
 
 const CourseDetails = () => {
-  const { id } = useParams()
-  const [courseData, setCourseData] = useState(null)
-  const { allCourses } = useContext(AppContext)
+  const { id } = useParams();
+  const [courseData, setCourseData] = useState(null);
+  const { allCourses } = useContext(AppContext);
 
   const fetchCourseData = async () => {
-    const findCourse = allCourses.find(course => course._id === id)
-    setCourseData(findCourse)
-  }
+    const findCourse = allCourses.find(course => course._id === id);
+    setCourseData(findCourse);
+  };
 
   useEffect(() => {
-    fetchCourseData()
-  }, [id, allCourses])
+    fetchCourseData();
+  }, [id, allCourses]);
 
-  return (
-    <div>
-      <h1>Course Details Page</h1>
-      {courseData ? (
-        <div>
-          <h2>{courseData.title}</h2>
-          <p>{courseData.description}</p>
-          
+  return courseData ? (
+    <>
+      <div className='flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left'>
+        <div className='absolute top-0 left-0 w-full h-section-height -z-1 bg-gradient-to-b from-cyan-100/70'></div>
+        <div className='max-w-xl z-10 text-gray-500'>
+          <h1 className='md:text-course-deatails-heading-large text-course-deatails-heading-small font-semibold text-gray-800'>
+            {courseData.courseTitle}
+          </h1>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: (courseData.courseDescription || '').slice(0, 200),
+            }}
+          ></p>
         </div>
-      ) : (
-        <p>Loading course details...</p>
-      )}
-    </div>
-  )
-}
+      </div>
+    </>
+  ) : (
+    <Loading />
+  );
+};
 
-export default CourseDetails
+export default CourseDetails;
