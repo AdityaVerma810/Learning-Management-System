1b586eb78ba854f7fec72bf99ca3fdb8e6dffdb1
AdityaVerma810 <adicode810@gmail.com>
2025-06-28 22:37:00 +0530

    creating a function to calculate course duration

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -49,6 +49,17 @@ const calculateChapterTie=(chapter)=>{
 	return humanizeDuration(time* 60 *1000,{units: ["h","m"]})
 
 }
+// function to calculate course duration
+const calculateCourseDuration=(course)=>{
+	let time=0
+
+	course.courseContent.map((chapter)=> chapter.chapterContent.map(
+		(lecture)=> time+=lecture.lectureDuration
+		
+	))
+	return humanizeDuration(time* 60 *1000,{units: ["h","m"]})
+}
+
 useEffect(()=>{
 	fetchAllCourses();
 },[]);
